f85e2fa1d3389baaff26225812dde797054b42e1
m4ll0k (mallok) <m4ll0k@protonmail.com>
2020-05-18 04:26:30 +0200

    Update aron.go

--- aron.go
+++ aron.go
@@ -7,7 +7,6 @@
 */
 
 package main
-
 import (
         "os"
 		"fmt"
@@ -18,23 +17,99 @@ import (
 		"strings"
 		"net/http"
 		"io/ioutil"
-		"github.com/m4ll0k/printer"
 )
 
+var BLACK   = "\033[1;30m"  // black
+var LBLACK  = "\033[0;30m"  // light black
+var RED     = "\033[1;31m" 
+var LRED    = "\033[0;31m" 
+var GREEN   = "\033[1;32m"
+var LGREEN  = "\033[0;32m"
+var YELLOW  = "\033[1;33m"
+var LYELLOW = "\033[0;33m"
+var BLUE    = "\033[1;34m"
+var LBLUE   = "\033[0;34m"
+var PURPLE  = "\033[1;35m"
+var LPURPLE = "\033[0;35m"
+var CYAN    = "\033[1;36m"
+var LCYAN   = "\033[0;36m"
+var GRAY    = "\033[1;37m"
+var LGRAY   = "\033[0;37m"
+var WHITE   = "\033[1;38m"
+var LWHITE  = "\033[0;38m"
+var RESET   = "\033[0m"
+// labels
+var INFO = "[i]"
+var PLUS = "[+]"
+var LESS = "[-]"
+var TEST = "[*]"
+var WARN = "[!]"
+var ASK  = "[?]"
+
+func Info(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",YELLOW,INFO,RESET,YELLOW,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",YELLOW,INFO,RESET,str)
+	}
+}
+//
+func Plus(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",GREEN,PLUS,RESET,GREEN,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",GREEN,PLUS,RESET,str)
+	}
+}
+//
+func Less(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",LRED,LESS,RESET,LRED,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",LRED,LESS,RESET,str)
+	}
+}
+//
+func Test(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",BLUE,TEST,RESET,BLUE,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",BLUE,TEST,RESET,str)
+	}
+}
+//
+func Warn(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",RED,WARN,RESET,RED,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",RED,WARN,RESET,str)
+	}
+}
+//
+func Ask(str string,color bool) {
+	if color{
+		fmt.Printf("%s%s%s %s%s%s\n",CYAN,ASK,RESET,CYAN,str,RESET)
+	} else{
+		fmt.Printf("%s%s%s %s\n",CYAN,ASK,RESET,str)
+	}
+}
+
 var (
-	_url      = flag.String("url","","\n\t\tSet target URL")
-	_get      = flag.Bool("get",false,"\n\t\tSet get method")
-	_post     = flag.Bool("post",false,"\n\t\tSet post method")
-	_data     = flag.String("data","","\n\t\tSet post data")
-	_wordlist = flag.String("wordlist","dict.txt","\n\t\tSet your wordlist")
+	_url      = flag.String("u","","\t\tSet target URL")
+	_get      = flag.Bool("g",false,"\t\tSet get method")
+	_post     = flag.Bool("p",false,"\t\tSet post method")
+	_data     = flag.String("d","","\t\tSet post data")
+	_headers  = flag.String("H","","\t\tSet headers ('name:value,name:value')")
+	_wordlist = flag.String("w","dict.txt","\t\tSet your wordlist")
+	_help     = flag.Bool("h",false,"\t\tShow this Help")
 )
 
 func Banner() {
-	fmt.Printf("%s    ___                         %s\n",printer.GREEN,printer.RESET)                   
-	fmt.Printf("%s   /   |  _________  ___       %s\n",printer.GREEN,printer.RESET)
-	fmt.Printf("%s  / /| | / ___/ __ \\/ __\\   %s\n",printer.GREEN,printer.RESET)
-	fmt.Printf("%s / ___ |/ /  / /_/ / / / /   %s\n",printer.GREEN,printer.RESET)
-	fmt.Printf("%s/_/  |_/_/   \\____/_/ /_/ %s(v0.1.0 beta)%s\n",printer.GREEN,printer.YELLOW,printer.RESET)
+	fmt.Printf("%s    ___                         %s\n",GREEN,RESET)                   
+	fmt.Printf("%s   /   |  _________  ___       %s\n",GREEN,RESET)
+	fmt.Printf("%s  / /| | / ___/ __ \\/ __\\   %s\n",GREEN,RESET)
+	fmt.Printf("%s / ___ |/ /  / /_/ / / / /   %s\n",GREEN,RESET)
+	fmt.Printf("%s/_/  |_/_/   \\____/_/ /_/ %s(v0.1.0 beta)%s\n",GREEN,YELLOW,RESET)
 	fmt.Printf("----------------------------\n")
 	fmt.Printf("  Momo (M4ll0k) Outaadi \n\n") 
 }
@@ -48,14 +123,12 @@ func CheckUrl(url string) string {
 			if strings.Contains(url,"."){
 				return "http://" + url
 			} else {
-				printer.Warn("Please enter with valid URL",true)
-				os.Exit(0)
+				Warn("Please enter with valid URL",true)
 				return ""
 			}
 		}
 	} else {
-		printer.Warn("Please enter with your URL!",true)
-		os.Exit(0)
+		Warn("Please enter with your URL!",true)
 		return ""
 	}
 	return ""
@@ -110,19 +183,19 @@ func JoinGet(url string, param string) string {
 }
 
 
-func Request(url string, method string, data string, payload string) {
+func Request(url string, method string, data string, payload string, headers string) {
 	// Do Request
 	m := strings.ToLower(method)
 	if m == "post" {
 		// 1 req content
-		f_content,f_url := Post(url,data)
+		f_content,f_url := Post(url,data,headers)
 		// 2 req content
-		s_content,s_url := Post(url,JoinPost(data,payload))
+		s_content,s_url := Post(url,JoinPost(data,payload),headers)
 		if s_content != f_content {
 			if s_url == url {
-				printer.Info("Found a potentially valid parameter: ",false)
-				printer.Plus("URL  => "+s_url,false)
-				printer.Plus("POST => "+JoinPost(data,payload),false)
+				Info("Found a potentially valid parameter: ",false)
+				Plus("URL  => "+s_url,false)
+				Plus("POST => "+JoinPost(data,payload),false)
 			} else {
 				if f_url == url {
 					fmt.Printf("")
@@ -131,14 +204,14 @@ func Request(url string, method string, data string, payload string) {
 		}
 	} else {
 		// 1 req content
-		f_content,f_url := Get(url)
+		f_content,f_url := Get(url,headers)
 		// 2 req content
 		real_url := JoinGet(url,payload)
-		s_content,s_url := Get(real_url)
+		s_content,s_url := Get(real_url,headers)
 		if s_content != f_content {
 			if s_url == real_url {
-				printer.Info("Found a potentially valid parameter: ",false)
-				printer.Plus("URL => " + s_url,false)
+				Info("Found a potentially valid parameter: ",false)
+				Plus("URL => " + s_url,false)
 			} else {
 				if s_url == f_url {
 					fmt.Printf("")
@@ -149,10 +222,18 @@ func Request(url string, method string, data string, payload string) {
 }
 
 
-func Get(url string) (string,string) {
+func Get(url string,headers string) (string,string) {
 	// create new request
 	req,err := http.NewRequest("GET",url,nil)
-	req.Header.Set("User-Agent","Mozilla/5.0")
+	req.Header.Set("User-Agent","curl/7.64.1")
+	// add headers
+	if headers != "" {
+		list := strings.Split(headers,",")
+		for _,value := range list {
+			r := strings.Split(value,":")
+			req.Header.Set(string(r[0]),string(r[1]))
+		}
+	}
 	// client
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
@@ -173,11 +254,19 @@ func Get(url string) (string,string) {
 	return "",""
 }
 
-func Post(url string, data string) (string,string) {
+func Post(url string, data string,headers string) (string,string) {
 	post_data := []byte(data)
 	// create new request
 	req, err := http.NewRequest("POST",url,bytes.NewBuffer(post_data))
-	req.Header.Set("User-Agent","Mozilla/5.0")
+	req.Header.Set("User-Agent","curl/7.64.1")
+	// add headers
+	if headers != "" {
+		list := strings.Split(headers,",")
+		for _,value := range list {
+			r := strings.Split(value,":")
+			req.Header.Set(string(r[0]),string(r[1]))
+		}
+	}
 	// client
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
@@ -195,42 +284,48 @@ func Post(url string, data string) (string,string) {
 
 func main() {
 	// Main 
-	Banner()
 	flag.Parse()
-	// -- time 
+	uri := *_url
+	help := *_help
+	// * 
+	if uri == "" || help == true {
+		flag.Usage()
+		return
+	}
+	Banner()
+	// time 
 	t := time.Now()
 	time_now := t.Format("2006-01-02 15:04:05")
-	printer.Plus("URL: "+*_url,false)
-	printer.Plus("Starting: "+time_now,false)
+	Plus("URL: "+*_url,false)
+	Plus("Starting: "+time_now,false)
 	fmt.Printf("\n")
-	//-- end time
+	// end time
 	file,err := os.Open(*_wordlist)
 	if err != nil {
-		printer.Warn("Error: \""+*_wordlist+"\" not found!!",true)
-		os.Exit(0)
+		Warn("Error: \""+*_wordlist+"\" not found!!",true)
+		return
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	//
-	uri := CheckUrl(*_url)
+	uri = CheckUrl(*_url)
 	if uri == "" {
-		flag.Usage()
-		os.Exit(0)
+		return
 	}
-	printer.Test("Bruteforcing...",true)
+	Test("Bruteforcing...",true)
+	headers := *_headers
 	if *_get {
 		// read payload
 		for scanner.Scan() {
-			Request(uri,"GET","",scanner.Text())
+			Request(uri,"GET","",scanner.Text(),headers)
 		}
-		// --
+		// 
 	} else {
 		if *_post { 
 			// read payload 
 			for scanner.Scan() {
-				Request(uri,"POST",*_data,scanner.Text())
+				Request(uri,"POST",*_data,scanner.Text(),headers)
 			}
 		}
 	}
-	printer.Test("Done!",true)
+	Test("Done!",true)
 }
